f4e7c30a5e8e2e2236cd8f6895864d5910272db7
= <=>
2022-03-04 19:22:25 -0300

    Updated logs model and command

--- commands/logs.js
+++ commands/logs.js
@@ -1,4 +1,4 @@
-const { Message, MessageEmbed } = require("discord.js");
+const { Message, MessageEmbed, TextChannel } = require("discord.js");
 const Log = require("../models/logs");
 module.exports = {
     name: "logs",
@@ -16,13 +16,7 @@ module.exports = {
         if (!member.permissions.has("MANAGE_GUILD")) return message.react(client.cemojis.no.emoji);
         const options = ["set", "off", "on"];
         lang = lang ? lang.lang : "es";
-        const responses = {
-            invalidOption: {
-                es: "La opción introducida es inválida",
-                en: "The entered option is invalid",
-                br: "A opção inserida é inválida"
-            }
-        }
+        const responses = {}
         /**
          * @param {number} length
          * @returns {string}
@@ -46,7 +40,41 @@ module.exports = {
             return char;
         }
         const option = args[0];
-        if (!option) return message.reply("```\n" + `${client.prefix}logs {option} {args}\n${createSpaces(client.prefix.length)}      ^^^^^^\n\nERR: Missing parameter` + "\n```");
+        if (!option) return message.reply("```\n" + `${client.prefix}logs {option} {args}\n${createSpaces(client.prefix.length)}      ^^^^^^\n\nERR: Missing Parameter` + "\n```");
         if (!options.some(o => o === option.toLowerCase())) return message.reply("```\n" + `${client.prefix}logs ${option} {args}\n${createSpaces(client.prefix.length)}     ${createArrows(option.length)}\n\nERR: Invalid Option` + "\n```");
+        let foundG = await Log.findOne({ guildId: guild.id });
+        if (!foundG) {
+            foundG = new Log({ guildid: guild.id, active: true });
+        }
+        const langs = ["es", "en", "br"];
+        switch (option.toLowerCase()) {
+            case "set": {
+                /**
+                 * @type {TextChannel}
+                 */
+                let ch = message.mentions.channels.first();
+                if (!ch) return message.reply("```\n" + `${client.prefix}logs set {channel}\n${createSpace(client.prefix.length)}          ^^^^^^^\n\nERR: Missing Parameter` + "\n```");
+                let lng = args[2];
+                if (!lng) return message.reply("```\n" + `${client.prefix}logs set #${ch.name} {lang}\n${createSpaces(client.prefix.length)}          ${createSpaces(ch.name.length)}  ^^^^\n\nERR: Missing Parameter` + "\n```");
+                if (!langs.some(l => l === lng.toLowerCase())) return message.reply("```\n" + `${client.prefix}logs set #${ch.name} ${lng}\n${createSpaces(client.prefix.length)}          ${createSpaces(ch.name.length)} ${createArrows(lng.length)}\n\nERR: Invalid Lang` + "\n```");
+                foundG.lang = lng.toLowerCase();
+                foundG.channelid = ch.id;
+                await foundG.save();
+                await message.react(client.cemojis.thumbup);
+                break;
+            }
+            case "on": {
+                if (foundG.active) return message.reply("```\n" + `${client.prefix}logs on\n${createSpaces(client.prefix.length)}     ^^\n\nERR: Logs already on` + "\n```");
+                foundG.active = true;
+                await foundG.save();
+                break;
+            }
+            case "off": {
+                if (!foundG.active) return message.reply("```\n" + `${client.prefix}logs off\n${createSpaces(client.prefix.length)}     ^^^\n\nERR: Logs already off` + "\n```");
+                foundG.active = false;
+                await foundG.save();
+                break;
+            }
+        }
     }
 }
--- models/logs.js
+++ models/logs.js
@@ -2,7 +2,7 @@ const { Schema, model } = require("mongoose");
 const LogSchema = new Schema({
     guildid: "",
     lang: "",
-    channeld: "",
+    channelid: "",
     active: Boolean
 });
 module.exports = model("logs", LogSchema);
