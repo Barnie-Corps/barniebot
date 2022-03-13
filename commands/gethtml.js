04d68e9fab9fcb81b861d29a3a6dfb5a532b6552
= <=>
2022-03-12 22:23:54 -0300

    Updated gethtml command

--- commands/gethtml.js
+++ commands/gethtml.js
@@ -5,6 +5,19 @@ module.exports = {
   description: 'gets the html code of a page',
   aliases: ['htmlget', 'get-html', 'html-get'],
   execute: async function(message, args, lang) {
+    /**
+     * @param {string} text
+     * @returns {boolean}
+     */
+    function isJsonFormat(text) {
+      try {
+        JSON.parse(text);
+      }
+      catch (err) {
+        return false;
+      }
+      return true;
+    }
     let nourl = 'Debes introducir la URL.';
     let nohttp = 'La URL debe ser absoluta.';
     if (lang !== null){
@@ -29,18 +42,22 @@ module.exports = {
         403: "Forbbiden",
         401: "Not authorized",
         429: "Too many requests (ratelimit)",
-        500: "Internal server error"
+        500: "Internal server error",
+        405: "Method not allowed (GET)"
       }
       if (res.status > 399) return message.reply(`${res.status} - ${errorMessages[res.status.toString()] ? errorMessages[res.status.toString()] : "Unknown error type"}`);
       const response = await res.text();
-      const filePath = '../page.html';
+      const paths = { txt: "../page.html", json: "../response.json" };
+      let filePath = "";
+      if (isJsonFormat(response)) filePath = paths.json;
+      else filePath = paths.txt;
       fs.writeFileSync(filePath, 'Loading...');
       const waitmsg = await message.reply('<a:discordproloading:875107406462472212>').catch(err => message.channel.send(`Error while replying\n\`${err}\``));
       setTimeout(async function(){
         fs.writeFileSync(filePath, response);
         await waitmsg.edit({ content: `Status ${res.status}`, files: [filePath] });
         setTimeout(function(){
-          fs.writeFileSync(filePath, '------- Waiting for requests -------');
+          fs.writeFileSync(filePath, isJsonFormat(response) ? JSON.stringify({ status: "Waiting for requests" }) : '------- Waiting for requests -------');
         }, 5000);
       }, 3000);
     }).catch(console.log);
