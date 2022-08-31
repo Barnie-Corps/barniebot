14dae1f5f14a2efd3bf71388d6b22ef327072879
santiadjmc <santiadjmc@gmail.com>
2022-08-31 15:16:24 -0500

    Fixed utils methods

--- utils.ts
+++ utils.ts
@@ -1,14 +1,14 @@
 const utils = {
     createArrows: (length: number): string => {
         let arrows = "";
-        for (let i = 0; i <= length; i++) {
+        for (let i = 0; i < length; i++) {
             arrows += "^"
         }
         return arrows;
     },
     createSpaces: (length: number): string => {
         let spaces = "";
-        for (let i = 0; i <= length; i++) {
+        for (let i = 0; i < length; i++) {
             spaces += " ";
         }
         return spaces;
