86d431ffe94b4a213b56ff7a97740ada693898b4
santiadjmc <santiadjmc@gmail.com>
2023-02-05 22:15:14 +0100

    Added package

---
+++ commands/meme.ts
@@ -0,0 +1,43 @@
+import { ActionRowBuilder, ButtonBuilder, ButtonStyle, ChatInputCommandInteraction, EmbedBuilder, SlashCommandBuilder } from "discord.js";
+import fetch from "node-fetch";
+import data from "../data";
+
+export default {
+    data: new SlashCommandBuilder()
+    .setName("meme")
+    .setDescription("Shows a random meme"),
+    async execute (interaction: ChatInputCommandInteraction, lang: string) {
+        await interaction.deferReply();
+        async function getMeme() {
+            let rsp = await fetch("https://meme-api.com/gimme");
+            let json = await rsp.json();
+            return new Promise(async (resolve, reject) => {
+                do {
+                    rsp = await fetch("https://meme-api.com/gimme");
+                    json = await rsp.json();
+                }
+                while (json.nsfw);
+                resolve(json);
+            });
+        }
+        const meme: any = await getMeme();
+        const row = new ActionRowBuilder()
+        .addComponents(
+            new ButtonBuilder()
+            .setStyle(ButtonStyle.Link)
+            .setLabel(`/r/${meme.subreddit}`)
+            .setURL(`https://reddit.com/r/${meme.subreddit}`),
+            new ButtonBuilder()
+            .setStyle(ButtonStyle.Link)
+            .setLabel("View post")
+            .setURL(meme.postLink)
+        )
+        const embed = new EmbedBuilder()
+        .setTitle(`${meme.title}`)
+        .setImage(meme.url)
+        .setColor("Purple")
+        .setTimestamp()
+        .setFooter({ text: `${data.bot.emojis[0].emoji} ${meme.ups}` })
+        await interaction.editReply({ embeds: [embed], components: [row as any] });
+    }
+}
