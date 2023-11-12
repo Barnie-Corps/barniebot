8f43f36916a9142d90b5b91e2d64209d125551aa
santiadjmc <santiadjmc@gmail.com>
2023-11-12 16:12:14 -0600

    Added dev command gethtml

---
+++ commands/gethtml.ts
@@ -0,0 +1,34 @@
+import { ChatInputCommandInteraction, EmbedBuilder, SlashCommandBuilder } from "discord.js";
+import * as fs from "fs";
+import utils from "../utils";
+
+export default {
+    data: new SlashCommandBuilder()
+        .setName("gethtml")
+        .setDescription("Gets the HTML and code response from a given URL.")
+        .addStringOption(o => o.setName("url").setRequired(true).setDescription("URL whose response code and html you wanna get")),
+    execute: async (interaction: ChatInputCommandInteraction, language: string) => {
+        let texts = {
+            code: "Código de respuesta",
+            invalid: "Ooops... Comprueba nuevamente la URL que me diste :)"
+        }
+        if (language !== "es") {
+            texts = await utils.autoTranslate(texts, "es", language);
+        }
+        const url = interaction.options.getString("url") as string;
+        let response;
+        try {
+            response = await fetch(url);
+        }
+        catch (err: any) {
+            interaction.editReply(texts.invalid);
+            return;
+        }
+        if (!response) return;
+        const html = await response.text();
+        const code = response.status;
+        fs.writeFileSync("../response.html", html);
+        await interaction.editReply({ content: `${texts.code}: ${code}\nHTML:`, files: ["../response.html"] });
+        fs.unlinkSync("../response.html");
+    }
+}
