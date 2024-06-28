9370229eaa7e17d800a6fa3c257a3b76f2f72079
santiadjmc <santiadjmc@gmail.com>
2024-06-27 22:59:05 -0400

    Updated LogManager

--- managers/LogManager.ts
+++ managers/LogManager.ts
@@ -15,28 +15,28 @@ class LogManager {
         if (!sources.includes("system")) this.sources.push("system");
         if (sources.length < 1) this.warn("log-manager", "There are no sources given to the constructor, only the following sources will be accepted: log-manager, unknown");
     }
-    error(source: string, message: string, send?: boolean): void {
+    error(source: string, message: string, send: boolean = false): void {
         if (typeof source !== "string") throw new TypeError(`Source parameter must be of type string, received ${typeof source}`);
         if (typeof message !== "string") throw new TypeError(`Source parameter must be of type string, received ${typeof message}`);
         if (!this.sources.some(s => s.toLowerCase() === source.toLowerCase())) throw new RangeError("Unknown source");
         console.log(`[${source.toUpperCase()}][ERROR]: ${message}`.red);
         if (send) this.send(data.bot.log_channel, `[${source.toUpperCase()}][ERROR]: ${message}`);
     }
-    info(source: string, message: string, send?: boolean): void {
+    info(source: string, message: string, send: boolean = false): void {
         if (typeof source !== "string") throw new TypeError(`Source parameter must be of type string, received ${typeof source}`);
         if (typeof message !== "string") throw new TypeError(`Source parameter must be of type string, received ${typeof message}`);
         if (!this.sources.some(s => s.toLowerCase() === source.toLowerCase())) throw new RangeError("Unknown source");
         console.log(`[${source.toUpperCase()}][INFO]: ${message}`.gray);
         if (send) this.send(data.bot.log_channel, `[${source.toUpperCase()}][INFO]: ${message}`);
     }
-    success(source: string, message: string, send?: boolean): void {
+    success(source: string, message: string, send: boolean = false): void {
         if (typeof source !== "string") throw new TypeError(`Source parameter must be of type string, received ${typeof source}`);
         if (typeof message !== "string") throw new TypeError(`Source parameter must be of type string, received ${typeof message}`);
         if (!this.sources.some(s => s.toLowerCase() === source.toLowerCase())) throw new RangeError("Unknown source");
         console.log(`[${source.toUpperCase()}][SUCCESS]: ${message}`.green);
         if (send) this.send(data.bot.log_channel, `[${source.toUpperCase()}][SUCCESS]: ${message}`);
     }
-    warn(source: string, message: string, send?: boolean): void {
+    warn(source: string, message: string, send: boolean = false): void {
         if (typeof source !== "string") throw new TypeError(`Source parameter must be of type string, received ${typeof source}`);
         if (typeof message !== "string") throw new TypeError(`Source parameter must be of type string, received ${typeof message}`);
         if (!this.sources.some(s => s.toLowerCase() === source.toLowerCase())) throw new RangeError("Unknown source");
