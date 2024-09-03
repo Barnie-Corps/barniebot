f5e417f7723340010b642b4ef0640270687805e8
santiadjmc <santiadjmc@gmail.com>
2024-09-02 22:06:21 -0400

    Update privacy command to include link to privacy.md file

--- commands/privacy.ts
+++ commands/privacy.ts
@@ -6,11 +6,11 @@ export default {
     .setName("privacy")
     .setDescription("Shows you the bot privacy policy."),
     execute: async (interaction: ChatInputCommandInteraction, lang: string) => {
-        let text = { value: "A continuación te enviaré mi política de privacidad, te recomiendo que la leas." };
+        let text = { value: "A continuación, te enviaré mi política de privacidad, te recomiendo que la leas." };
         if (lang !== "es") {
             text = (await utils.autoTranslate(text, "es", lang));
         }
-        await interaction.editReply(`${text.value}\n[privacy.txt](https://github.com/Barnie-Corps/barniebot/blob/master/privacy.txt)`);
+        await interaction.editReply(`${text.value}\n[privacy.md](https://github.com/Barnie-Corps/barniebot/blob/master/privacy.md)`);
     },
     ephemeral: false
 }
