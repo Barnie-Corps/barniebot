4c81d0e4e4fe15a4f20d6c3cde13b8123dc0baa0
santiadjmc <santiadjmc@gmail.com>
2024-09-07 16:12:25 -0400

    Refactor filter_words table creation query in queries.ts

--- mysql/queries.ts
+++ mysql/queries.ts
@@ -10,7 +10,7 @@ export default function queries(): void {
     db.query("CREATE TABLE IF NOT EXISTS guilds (id VARCHAR(255) NOT NULL, name VARCHAR(255) NOT NULL, member_count INT(255) NOT NULL DEFAULT 2, is_in BOOLEAN NOT NULL DEFAULT TRUE)");
     db.query("CREATE TABLE IF NOT EXISTS global_bans (id VARCHAR(255) NOT NULL, active BOOLEAN NOT NULL DEFAULT TRUE, times INT(255) NOT NULL DEFAULT 1)");
     db.query("CREATE TABLE IF NOT EXISTS filter_configs (guild VARCHAR(255) NOT NULL, enabled BOOLEAN NOT NULL DEFAULT TRUE, log_channel VARCHAR(255) NOT NULL DEFAULT '0', enabled_logs BOOLEAN NOT NULL DEFAULT FALSE, lang VARCHAR(2) NOT NULL DEFAULT 'en')");
-    db.query("CREATE TABLE IF NOT EXISTS filter_words (id INT(255) NOT NULL PRIMARY KEY AUTO_INCREMENT, guild VARCHAR(255), content VARCHAR(255) NOT NULL DEFAULT '', protected BOOLEAN NOT NULL DEFAULT FALSE, single BOOLEAN NOT NULL DEFAULT TRUE)"); // Single means the word must be alone in the message, not part of another word
+    db.query("CREATE TABLE IF NOT EXISTS filter_words (id INT(255) NOT NULL PRIMARY KEY AUTO_INCREMENT, guild VARCHAR(255) NOT NULL, content VARCHAR(255) NOT NULL DEFAULT '', protected BOOLEAN NOT NULL DEFAULT FALSE, single BOOLEAN NOT NULL DEFAULT TRUE)"); // Single means the word must be alone in the message, not part of another word
     db.query("CREATE TABLE IF NOT EXISTS filter_webhooks (id VARCHAR(255) NOT NULL, token VARCHAR(255) NOT NULL, channel VARCHAR(255) NOT NULL)");
     db.query("CREATE TABLE IF NOT EXISTS vip_users (id VARCHAR(255) NOT NULL, start_date BIGINT(255) NOT NULL, end_date BIGINT(255) NOT NULL)");
     db.query("CREATE TABLE IF NOT EXISTS ai_history (id INT PRIMARY KEY AUTO_INCREMENT, content TEXT NOT NULL, uid VARCHAR(255) NOT NULL)");
