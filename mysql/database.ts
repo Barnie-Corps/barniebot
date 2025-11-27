83dd10bd348a460458874df3cee7940f69a74635
santiadjmc <santiadjmc@gmail.com>
2025-11-27 00:42:01 -0600

    Added database connections pooling

--- mysql/database.ts
+++ mysql/database.ts
@@ -1,7 +1,29 @@
 import data from "../data";
-import { createConnection } from "mysql";
-const db = createConnection(data.database);
-db.connect();
-db.query = require("util").promisify(db.query);
+import { createPool } from "mysql";
+
+const db = createPool({
+    ...data.database,
+    connectionLimit: 10,
+    waitForConnections: true,
+    queueLimit: 0,
+    connectTimeout: 10000,
+    acquireTimeout: 10000,
+    timeout: 60000
+});
+
+(db as any).query = require("util").promisify(db.query);
+
+db.on('error', (err) => {
+    console.error('MySQL Pool Error:', err);
+    if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+        console.error('Database connection was closed.');
+    }
+    if (err.code === 'ER_CON_COUNT_ERROR') {
+        console.error('Database has too many connections.');
+    }
+    if (err.code === 'ECONNREFUSED') {
+        console.error('Database connection was refused.');
+    }
+});
 
 export default db;
